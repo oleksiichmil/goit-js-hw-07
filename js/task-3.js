6fdb3bde0e04d33668bf2ec4511338a8377e39d4
Oleksii Chmil <66308922+oleksiichmil@users.noreply.github.com>
2025-01-19 16:19:49 +0200

    add file

---
+++ js/task-3.js
@@ -0,0 +1,9 @@
+const inputEl = document.querySelector('#name-input');
+const outputEl = document.querySelector('#name-output');
+
+inputEl.addEventListener('input', event => {
+
+const trimmedValue = event.target.value.trim();
+
+outputEl.textContent = trimmedValue === '' ? 'Anonymous' : trimmedValue;
+});
